3ea8fc0627ed5af3bda8a21963c9f0e257f96804
ariyapooh <s6406021621137@fitm.kmutnb.ac.th>
2024-02-14 08:57:43 +0700

    .

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const sequelize = new Sequelize('database', 'username', 'password', {
     host: 'localhost',
     dialect: 'sqlite',
     storage: './Database/SQBooks.sqlite'
+    //
 });
 
 const Book = sequelize.define('book', {
